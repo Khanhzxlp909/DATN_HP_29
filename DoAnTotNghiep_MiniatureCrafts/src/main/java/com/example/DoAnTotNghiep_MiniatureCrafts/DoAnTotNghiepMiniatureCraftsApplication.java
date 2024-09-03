3a5630c536fa9fd5b5800423c1885fa7085785e8
Khanhzxlp909 <157184716+Khanhzxlp909@users.noreply.github.com>
2024-09-03 20:19:39 +0700

    fist commit

---
+++ DoAnTotNghiep_MiniatureCrafts/src/main/java/com/example/DoAnTotNghiep_MiniatureCrafts/DoAnTotNghiepMiniatureCraftsApplication.java
@@ -0,0 +1,13 @@
+package com.example.DoAnTotNghiep_MiniatureCrafts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAnTotNghiepMiniatureCraftsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoAnTotNghiepMiniatureCraftsApplication.class, args);
+	}
+
+}
---
+++ DoAnTotNghiep_MiniatureCrafts/src/test/java/com/example/DoAnTotNghiep_MiniatureCrafts/DoAnTotNghiepMiniatureCraftsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DoAnTotNghiep_MiniatureCrafts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAnTotNghiepMiniatureCraftsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
