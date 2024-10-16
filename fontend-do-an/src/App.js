78a3228cff9697f32ed4276ed2115b4e9cac82ca
Khanh Pham <khanhpham422004@gmail.com>
2024-10-16 22:29:31 +0700

    f

--- fontend-do-an/src/App.js
+++ fontend-do-an/src/App.js
@@ -1,23 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
+
 
 function App() {
+   const age = 18;
+   const name = "Eat3torice";
+   const isFemale = true;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Eat3torice
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1> Hello World </h1>
+        <p> Xin chao {name} dep trai - tuoi {age} -{isFemale ? 'Male' : 'Female'}</p>
+
+        {isFemale ? <p>Female</p> : <p>Male</p>}
+
+        {isFemale && <p>Female</p>}
+
+        {isFemale && <p>Male</p>}
+
+        
       </header>
     </div>
   );
